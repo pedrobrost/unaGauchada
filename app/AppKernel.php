803f6e7d9954d1617ddfe7e94d708545bd95a2fa
Pedro Brost <pedro.brost97@gmail.com>
2017-05-20 15:51:26 -0300

    Se creo la lista de publicaciones

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new UnaGauchada\ClientBundle\ClientBundle(),
+            new UnaGauchada\PublicationBundle\PublicationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
