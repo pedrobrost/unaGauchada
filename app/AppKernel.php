98598be8019f1581700ee0819ba7ebcc50eead7e
Pedro Brost <pedro.brost97@gmail.com>
2017-05-21 14:50:24 -0300

    Se eliminio el ClientBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new UnaGauchada\ClientBundle\ClientBundle(),
             new UnaGauchada\PublicationBundle\PublicationBundle(),
         ];
 
--- src/UnaGauchada/ClientBundle/ClientBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UnaGauchada\ClientBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ClientBundle extends Bundle
-{
-}
