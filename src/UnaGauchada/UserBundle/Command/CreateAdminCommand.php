680c5b0c52e08ff5463b16ad690f151b48b01521
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-23 12:16:54 -0300

    rename CreateUserCommand to CreateAdminCommand

--- src/UnaGauchada/UserBundle/Command/CreateAdminCommand.php
+++ src/UnaGauchada/UserBundle/Command/CreateAdminCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class CreateUserCommand extends Command {
+class CreateAdminCommand extends Command {
 
     protected function configure(){
         $this
