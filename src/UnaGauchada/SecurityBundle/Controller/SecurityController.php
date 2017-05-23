7b4de6be29229b696670c7abbab3aae568f68a4b
Pedro Brost <pedro.brost97@gmail.com>
2017-05-23 18:58:52 -0300

    Se comenzo con el registro

--- src/UnaGauchada/SecurityBundle/Controller/SecurityController.php
+++ src/UnaGauchada/SecurityBundle/Controller/SecurityController.php
@@ -3,6 +3,8 @@
 namespace UnaGauchada\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use UnaGauchada\UserBundle\Entity\User;
 
 class SecurityController extends Controller
 {
@@ -11,7 +13,32 @@ class SecurityController extends Controller
         $authUtils = $this->get('security.authentication_utils');
         $error = $authUtils->getLastAuthenticationError();
         $lastUsername = $authUtils->getLastUsername();
-        return $this->render('UserBundle:Login:login.html.twig', array('error' => $error, 'lastUsername' => $lastUsername));
+        return $this->render('UGSecurityBundle:Login:login.html.twig', array('error' => $error, 'lastUsername' => $lastUsername));
+    }
+
+    public function registerAction(){
+        return $this->render('UGSecurityBundle:Register:register.html.twig');
+    }
+
+    public function signupAction(Request $request){
+
+        // create the user
+        $user = new User();
+        $user
+            ->setName($request->get('name'))
+            ->setLastName($request->get('lastName'))
+            ->setEmail($request->get('email'))
+            ->setIsAdmin(false)
+            ->setPlainPassword($request->get('password'))
+            ->setPassword('chunk')
+            ->setSalt('chunk')
+            ->setBirthday(new \DateTime($request->get('birthday')));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->redirectToRoute('publication_homepage');
     }
 
 }
--- src/UnaGauchada/UserBundle/Service/PasswordUpdaterService.php
+++ src/UnaGauchada/UserBundle/Service/PasswordUpdaterService.php
@@ -22,22 +22,16 @@ class PasswordUpdaterService{
             return;
         }
 
-        $encoder = $this->encoderFactory->getEncoder($user);
-
         $user->setSalt(rtrim(str_replace('+', '.', base64_encode(random_bytes(32))), '='));
 
-        $hashedPassword = $encoder->encodePassword($plainPassword, $user->getSalt());
+        $hashedPassword = $this->encodePassword($user, $user->getPlainPassword());
         $user->setPassword($hashedPassword);
         $user->eraseCredentials();
     }
 
     public function encodePassword($user, $pass){
         $encoder = $this->encoderFactory->getEncoder($user);
-
-        $user->setSalt(rtrim(str_replace('+', '.', base64_encode(random_bytes(32))), '='));
-
         return $encoder->encodePassword($pass, $user->getSalt());
-
     }
 
 }
