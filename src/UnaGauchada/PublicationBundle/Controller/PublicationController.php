9e93fe4af5c34401df958938f6441d4acc0cd2d9
francoborrelli <francoborrelli96@outlook.com>
2017-05-23 13:41:04 -0300

    Se realizó la navbarAdmin y se modificó la pagina de inicio

--- src/UnaGauchada/PublicationBundle/Controller/PublicationController.php
+++ src/UnaGauchada/PublicationBundle/Controller/PublicationController.php
@@ -27,12 +27,17 @@ class PublicationController extends Controller
             ),
             new Publication(
                 'Mila para Luis',
-                'img/logo.png',
+                'img/logocard.jpg',
                 new \DateTime()
             ),
             new Publication(
                 'Mila para Luis',
                 'http://car-bone.pl/wp-content/uploads/2015/01/carbone_poster_100x70_porsche_935_4a_vertical_prev.png',
+                new \DateTime()
+            ),
+            new Publication(
+                'Mila para Luis',
+                'http://lorempixel.com/600/337/?time=',
                 new \DateTime()
             )
         );
