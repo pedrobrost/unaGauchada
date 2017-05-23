0fa63ffb760b360624ed279e297d45c5d5b1308b
francoborrelli <francoborrelli96@outlook.com>
2017-05-22 21:28:30 -0300

    Se agregó la view de loggin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new UnaGauchada\PublicationBundle\PublicationBundle(),
+            new UnaGauchada\ClientTestBundle\ClientTestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/UnaGauchada/ClientTestBundle/ClientTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace UnaGauchada\ClientTestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ClientTestBundle extends Bundle
+{
+}
