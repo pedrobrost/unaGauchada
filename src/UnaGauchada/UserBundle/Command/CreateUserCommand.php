0d2258e0880738f8721c79721502f5d5c34c958d
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-23 12:16:14 -0300

    create the new admin command

--- src/UnaGauchada/UserBundle/Command/CreateUserCommand.php
+++ src/UnaGauchada/UserBundle/Command/CreateUserCommand.php
@@ -1,15 +1,57 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: matias
- * Date: 5/23/17
- * Time: 10:59 AM
- */
-
 namespace UnaGauchada\UserBundle\Command;
 
 
-class CreateUserCommand
-{
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CreateUserCommand extends Command {
+
+    protected function configure(){
+        $this
+            ->setName('app:create:admin')
+            ->setDescription('Create a new User Admin')
+            ->setHelp('This command creates a new administrator');
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $io = new SymfonyStyle($input, $output);
+        $io->newLine();
+        $io->block('Welcome to the UnaGauchada user creator', null, 'bg=blue;fg=white', ' ', true);
+        $io->newLine();
+
+        $email = $io->ask('User email', null, function($email){
+            if (!is_string($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                throw new \RuntimeException(
+                    'The email is invalid'
+                );
+            }
+
+            return $email;
+        });
+
+        $password = $io->askHidden('User password', null, function ($password) use ($io){
+            if( !is_string($password) ){
+                throw new \RuntimeException(
+                    'Invalid Password'
+                );
+            }
+        });
+
+
+        $output->writeln($email);
+        $output->writeln($password);
+
+        // create the user
+    }
 
 }
