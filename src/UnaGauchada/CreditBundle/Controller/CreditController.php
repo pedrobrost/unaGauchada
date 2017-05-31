78647e850962a151cc0d56f9e7a5b9acb3561e0c
Pedro Brost <pedro.brost97@gmail.com>
2017-05-31 16:13:31 -0300

    Se valida la fecha de vencimiento de la tarjeta

--- src/UnaGauchada/CreditBundle/Controller/CreditController.php
+++ src/UnaGauchada/CreditBundle/Controller/CreditController.php
@@ -17,13 +17,13 @@ class CreditController extends Controller
         return $this->render('CreditBundle::userInfo.html.twig', array('amount' => $request->get('amount')));
     }
 
-    public function summaryAction(Request $request)
+    public function summaryAction()
     {
         return $this->render('CreditBundle::summary.html.twig');
     }
 
     public function buyAction(Request $request){
-        if($request->get('creditCard') != '123456'){
+        if($request->get('creditCard') != '123456' | $this->invalidDate($request->get('month'), $request->get('year'))){
             return $this->render('CreditBundle::success.html.twig', array('success' => false));
         }
         $repository = $this->getDoctrine()->getRepository('CreditBundle:TransactionReason');
@@ -33,7 +33,13 @@ class CreditController extends Controller
         $em = $this->getDoctrine()->getManager();
         $em->persist($this->getUser());
         $em->flush();
+
+
         return $this->render('CreditBundle::success.html.twig', array('success' => true));
+    }
+
+    public function invalidDate($month, $year){
+        return ($year == date("Y") && $month <= date("m"));
     }
 
 }
