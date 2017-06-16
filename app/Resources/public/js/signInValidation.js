ee48bcaf70db932917b5da482c71e181079d4128
francoborrelli <francoborrelli96@outlook.com>
2017-06-16 16:13:47 -0300

    se saco un comentario

--- app/Resources/public/js/signInValidation.js
+++ app/Resources/public/js/signInValidation.js
@@ -1,13 +1,7 @@
 $('#signInForm').validate({
-            // Specify validation rules
             rules: {
-                // The key name on the left side is the name attribute
-                // of an input field. Validation rules are defined
-                // on the right side
                 _username: {
                     required: true,
-                    // Specify that email should be validated
-                    // by the built-in "email" rule
                     email: true
                 },
                 _password: {
@@ -15,7 +9,6 @@ $('#signInForm').validate({
                     required: true,
                 },
             },
-            // Specify validation error messages
             messages: {
                 _password: {
                     required: "Por favor ingrese su contraseña",
@@ -36,8 +29,6 @@ $('#signInForm').validate({
                 $(element).removeClass('form-control-warning');
                 $(element).closest('.form-group').addClass('has-success');
             },
-            // Make sure the form is submitted to the destination defined
-            // in the "action" attribute of the form when valid
             submitHandler: function (form) {
                 form.submit();
             },
