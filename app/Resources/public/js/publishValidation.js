34d830b39670b0fbc8278dd52af3889e363f4f0c
francoborrelli <francoborrelli96@outlook.com>
2017-06-16 20:29:20 -0300

    re realizó refactoring sobre los comentarios

--- app/Resources/public/js/publishValidation.js
+++ app/Resources/public/js/publishValidation.js
@@ -1,10 +1,7 @@
 
 $('#createPublication').validate({
-            // Specify validation rules
             rules: {
-                // The key name on the left side is the name attribute
-                // of an input field. Validation rules are defined
-                // on the right side
+
                 title: {
                     required: true,
                     maxlength: 50,
@@ -22,7 +19,6 @@ $('#createPublication').validate({
                     required: true,  
                 },
             },
-            // Specify validation error messages
             messages: {
                 title: {
                     required: "Por favor ingrese el título",
@@ -53,8 +49,6 @@ $('#createPublication').validate({
                 $(element).removeClass('form-control-warning');
                 $(element).closest('.form-group').addClass('has-success');
             },
-            // Make sure the form is submitted to the destination defined
-            // in the "action" attribute of the form when valid
             submitHandler: function (form) {
                 form.submit();
             },
