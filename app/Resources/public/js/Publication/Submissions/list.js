32bfd97c3b8ea92e6d02b8582a8b991991cbc3a0
francoborrelli <francoborrelli96@outlook.com>
2017-06-18 00:47:54 -0300

    se creo la pantalla de postulados

---
+++ app/Resources/public/js/Publication/Submissions/list.js
@@ -0,0 +1,40 @@
+$(function () {
+    if (window.location == window.parent.location) {
+        $('#back-to-bootsnipp').removeClass('hide');
+    }
+    
+    
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    $('#fullscreen').on('click', function(event) {
+        event.preventDefault();
+        window.parent.location = "http://bootsnipp.com/iframe/4l0k2";
+    });
+    $('a[href="#cant-do-all-the-work-for-you"]').on('click', function(event) {
+        event.preventDefault();
+        $('#cant-do-all-the-work-for-you').modal('show');
+    })
+    
+    $('[data-command="toggle-search"]').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('hide-search');
+        
+        if ($(this).hasClass('hide-search')) {        
+            $('.c-search').closest('.search').slideUp(100);
+        }else{   
+            $('.c-search').closest('.search').slideDown(100);
+        }
+    })
+    
+    $('#contact-list').searchable({
+        searchField: '#contact-list-search',
+        selector: 'li',
+        childSelector: '.col-xs-12',
+        show: function( elem ) {
+            elem.slideDown(100);
+        },
+        hide: function( elem ) {
+            elem.slideUp( 100 );
+        }
+    })
+});
--- src/UnaGauchada/PublicationBundle/Controller/PublicationController.php
+++ src/UnaGauchada/PublicationBundle/Controller/PublicationController.php
@@ -32,8 +32,13 @@ class PublicationController extends Controller
         return $this->render('PublicationBundle:Publications:publication.html.twig', array('publication' => $publication));
     }
 
+    //public function publishAction(){
+    //   return $this->render('PublicationBundle:Creation:creation.html.twig');
+    //}
+
+
     public function publishAction(){
-       return $this->render('PublicationBundle:Creation:creation.html.twig');
+       return $this->render('PublicationBundle:Submissions:list.html.twig');
     }
 
     public function publishCreateAction(Request $request){
