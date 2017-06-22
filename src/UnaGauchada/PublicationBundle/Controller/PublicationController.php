05c9f13b05bdfadc58345c1a3e452b9def150035
pedrobrost <pedro.brost97@gmail.com>
2017-06-21 23:55:31 -0300

    se agrego la funcionalidad al boton ver postulados para la demo

--- src/UnaGauchada/PublicationBundle/Controller/PublicationController.php
+++ src/UnaGauchada/PublicationBundle/Controller/PublicationController.php
@@ -32,14 +32,9 @@ class PublicationController extends Controller
         return $this->render('PublicationBundle:Publications:publication.html.twig', array('publication' => $publication));
     }
 
-    //public function publishAction(){
-    //  return $this->render('PublicationBundle:Creation:creation.html.twig');
-    //}
-
     public function publishAction(){
-      return $this->render('PublicationBundle:Submissions:list.html.twig');
+      return $this->render('PublicationBundle:Creation:creation.html.twig');
     }
-
 
     public function publishCreateAction(Request $request){
 
@@ -90,7 +85,7 @@ class PublicationController extends Controller
     }
 
     public function submissionsAction(Publication $publication){
-
+        return $this->render('PublicationBundle:Submissions:list.html.twig');
     }
 
 }
