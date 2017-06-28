d776d45060839ae460f98612f71c0058f65fd7ea
francoborrelli <francoborrelli96@outlook.com>
2017-06-28 11:35:11 -0300

    se arreglo el js de submissions

--- app/Resources/public/js/MySubmissions/mySubmissions.js
+++ app/Resources/public/js/MySubmissions/mySubmissions.js
@@ -1,30 +1,32 @@
 $(document).ready(function () {
 
-    $('#elegidoMessage').hide();
-    $('#rechazadoMessage').hide();
-    $('#pendienteMessage').hide();
+  $('#elegidoMessage').hide();
+  $('#rechazadoMessage').hide();
+  $('#pendienteMessage').hide();
 
-    var last;
+  var last;
 
-     $('.btn-filter').on('click', function () {
-       var $target = $(this).data('target');
-       var count = 0;
-       if ($target != 'all') {
-         $('li.list-group-item').hide();
+  $('.btn-filter').on('click', function () {
+    var $target = $(this).data('target');
+    var count = 0;
+    if ($target != 'all') {
+      $('li.list-group-item').hide();
 
-         $('li.list-group-item[data-status="' + $target + '"]').each(function() {
-  $( this ).fadeIn('slow');
-  count++;
-});
-        if (count == 0){$('#' + $target+ 'Message').fadeIn('slow');}
-       } else {
-         $('li.list-group-item').css('display', 'none').fadeIn('slow');
-             $('#elegidoMessage').hide();
-             $('#rechazadoMessage').hide();
-             $('#pendienteMessage').hide();
-       }
-       $('.btn-filter').removeClass('active');
-       $(this).addClass('active');
-     });
- 
+      $('li.list-group-item[data-status="' + $target + '"]').each(function () {
+        $(this).fadeIn('slow');
+        count++;
+      });
+      if (count == 0) {
+        $('#' + $target + 'Message').fadeIn('slow');
+      }
+    } else {
+      $('li.list-group-item').css('display', 'none').fadeIn('slow');
+      $('#elegidoMessage').hide();
+      $('#rechazadoMessage').hide();
+      $('#pendienteMessage').hide();
+    }
+    $('.btn-filter').removeClass('active');
+    $(this).addClass('active');
   });
+
+});
