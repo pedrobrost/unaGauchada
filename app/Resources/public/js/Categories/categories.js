917e54c191cfd308bf20eca152bba3fa19387ef9
francoborrelli <francoborrelli96@outlook.com>
2017-07-16 20:13:33 -0300

    searcher

--- app/Resources/public/js/Categories/categories.js
+++ app/Resources/public/js/Categories/categories.js
@@ -17,7 +17,6 @@ $(document).ready(function () {
   ],
   "bLengthChange": false,
           "info":     false,
-          "bFilter": false,
     "language": {
         "sProcessing":    "Procesando...",
         "sLengthMenu":    "Mostrar _MENU_ usuarios",
