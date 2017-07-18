b9605c0b43c0c0cf9a0c4be83941f30a6f44ba70
francoborrelli <francoborrelli96@outlook.com>
2017-07-18 12:47:28 -0300

    se cambio el formato del calendario

---
+++ app/Resources/public/js/Credits/pickerPage2.js
@@ -0,0 +1,51 @@
+$('#datePicker').daterangepicker({
+    "ranges": {
+        'Hoy': [moment(), moment()],
+        'Ayer': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Últimos 7 días': [moment().subtract(6, 'days'), moment()],
+        'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
+        'Este mes': [moment().startOf('month'), moment().endOf('month')],
+        'Mes pasado': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    "locale": {
+        "format": "DD/MM/YYYY",
+        "separator": " - ",
+        "applyLabel": "Aplicar",
+        "cancelLabel": "Cancelar",
+        "fromLabel": "Desde",
+        "toLabel": "Hasta",
+        "customRangeLabel": "Elegir fechas",
+        "daysOfWeek": [
+            "Do",
+            "Lu",
+            "Ma",
+            "Mi",
+            "Ju",
+            "Vi",
+            "Sa"
+        ],
+        "monthNames": [
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"
+        ],
+        "firstDay": 1
+    },
+    "showCustomRangeLabel": false,
+    "alwaysShowCalendars": true,
+    "linkedCalendars": false,
+    "minDate": "01/01/2017",
+    "maxDate": "31/12/2999",
+    "opens": "left"
+}, function (start, end, label) {
+    console.log("New date range selected: " + start.format('MM-DD-YYYY') + " to " + end.format('MM-DD-YYYY') + " (predefined range: " + label + ")");
+});
