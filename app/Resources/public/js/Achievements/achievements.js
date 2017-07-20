9c2ff23aaa5fd7874b60d5eb9ce6c58252c0cfe6
francoborrelli <francoborrelli96@outlook.com>
2017-07-20 19:21:52 -0300

    se agrego el infinito

--- app/Resources/public/js/Achievements/achievements.js
+++ app/Resources/public/js/Achievements/achievements.js
@@ -1,88 +1,89 @@
 var editando = false;
 var last = null;
 
-$('.submit').on('click', function (e) {
-    $(this).closest("tr").find('input').prop('readonly', true);
-    $(this).closest("tr").find('.editable').hide();
-    $(this).closest("tr").find('.botones').show();
-    editando = false;
-    if (btnAgregar = true){
-        $('.table-add').show();
-        btnAgregar = false;
-    }
-})
-
-
-$('.edit').on('click', function (e) {
-    if (editando == true){
-    last.find('input').prop('readonly', true);
-    last.find('form').trigger("reset");
-    $('.editable').hide();
-    $('.botones').show();   
-    }
-      $(this).closest("tr").find('input').prop('readonly', false);
-      $(this).closest("div").hide();
-      $(this).closest("tr").find('.editable').show();
-      editando = true;
-      last = $(this).closest("tr");
-})
-
-$('.editCancel').on('click', function (e) {
-    $(this).closest("tr").find('input').prop('readonly', true);
-    $(this).closest("tr").find('.editable').hide();
-    $(this).closest("tr").find('.botones').show();
-    editando = false;
-    if (btnAgregar = true){
-    $('.table-add').show();
+$(".submit").on("click", function(e) {
+  $(this).closest("tr").find("input").prop("readonly", true);
+  $(this).closest("tr").find(".editable").hide();
+  $(this).closest("tr").find(".botones").show();
+  editando = false;
+  if ((btnAgregar = true)) {
+    $(".table-add").show();
     btnAgregar = false;
-    $clone.remove();
-    }
-})
-
-var $TABLE = $('#logros');
-var btnAgregar = false;
-var $clone;
-$(document).ready(function () {
-    $("#mySubmissions").addClass("active");
-    $(".editable").hide();
-    $('input').prop('readonly', true);
-    $TABLE.find('tr.hide').hide();
+  }
 });
 
+$(".edit").on("click", function(e) {
+  if (editando == true) {
+    last.find("input").prop("readonly", true);
+    last.find("form").trigger("reset");
+    $(".editable").hide();
+    $(".botones").show();
+  }
+  $(this).closest("tr").find("input").prop("readonly", false);
+  $("#infinity").prop("readonly", true);
 
-$('td').on('keydown paste', function(event) { //Prevent on paste as well
-  if($(this).text().length === 10 && event.keyCode != 8) { 
+  $(this).closest("div").hide();
+  $(this).closest("tr").find(".editable").show();
+  editando = true;
+  last = $(this).closest("tr");
+});
+
+$(".editCancel").on("click", function(e) {
+  $(this).closest("tr").find("input").prop("readonly", true);
+  $(this).closest("tr").find(".editable").hide();
+  $(this).closest("tr").find(".botones").show();
+  editando = false;
+  if ((btnAgregar = true)) {
+    $(".table-add").show();
+    btnAgregar = false;
+    $clone.remove();
+  }
+});
+
+var $TABLE = $("#logros");
+var btnAgregar = false;
+var $clone;
+$(document).ready(function() {
+  $("#mySubmissions").addClass("active");
+  $(".editable").hide();
+  $("input").prop("readonly", true);
+  $TABLE.find("tr.hide").hide();
+});
+
+$("td").on("keydown paste", function(event) {
+  //Prevent on paste as well
+  if ($(this).text().length === 10 && event.keyCode != 8) {
     event.preventDefault();
   }
 });
 
-$('#range').keypress(function(e) {
-    if (isNaN(String.fromCharCode(e.which))) e.preventDefault();
+$("#range").keypress(function(e) {
+  if (isNaN(String.fromCharCode(e.which))) e.preventDefault();
 });
 
-$('.table-add').click(function () {
-  $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+$(".table-add").click(function() {
+  $clone = $TABLE.find("tr.hide").clone(true).removeClass("hide table-line");
   $clone.show();
-  $clone.find('input').prop('readonly', false);
-  $clone.find('.edit').trigger('click');
+  $clone.find("input").prop("readonly", false);
+  $clone.find(".edit").trigger("click");
   last = $clone;
   $TABLE.append($clone);
-  $('.table-add').hide();
+  $(".table-add").hide();
   btnAgregar = true;
   editando = true;
 });
 
-$('.table-remove').click(function () {
-  $(this).parents('tr').detach();
+$(".table-remove").click(function() {
+  $(this).parents("tr").detach();
 });
 
-$('.table-up').click(function () {
-  var $row = $(this).parents('tr');
+$(".table-up").click(function() {
+  var $row = $(this).parents("tr");
   if ($row.index() === 1) return; // Don't go above the header
   $row.prev().before($row.get(0));
 });
 
-$('.table-down').click(function () {
-  var $row = $(this).parents('tr');
+$(".table-down").click(function() {
+  var $row = $(this).parents("tr");
   $row.next().after($row.get(0));
 });
