02d1d639371a4bcce018d48dfc56c5b11e35d683
francoborrelli <francoborrelli96@outlook.com>
2017-07-23 14:54:41 -0300

    mejoras

--- app/Resources/public/js/EditProfile/editProfileValidation.js
+++ app/Resources/public/js/EditProfile/editProfileValidation.js
@@ -1,7 +1,3 @@
-$(document).ready( function() {
-        $("#modalbutton").hide();
-});
-
 $("#cancelform").click(function(){
     $("#editProfile_form").validate().resetForm();
     change = false;
@@ -21,56 +17,81 @@ $('#editProfile_form').change(function(){
 
 $.validator.addMethod("nonNumeric", function(value, element) {
     return this.optional(element) || !value.match(/[0-9]+/);
+})
+
+$(document).ready(function () {
+    $("#modalbutton").hide();
+    $('#name').popover({
+        placement: 'right',
+        trigger: 'manual'
+    });
+    $('#lastName').popover({
+        placement: 'right',
+        trigger: 'manual'
+    });
+    $('#email').popover({
+        placement: 'right',
+        trigger: 'manual'
+    });
+    $('#phone').popover({
+        placement: 'right',
+        trigger: 'manual'
+    });
 });
 
 $('#editProfile_form').validate({
-            rules: {
-                name: {
-                    required: true,
-                    nonNumeric: true,
-                },
-                lastName: {
-                    required: true,
-                    nonNumeric: true,
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                phone: {
-                    required: true,
-                    number: true,
-                },
-            },
-            messages: {
-                name: {
-                    required: "Por favor ingrese su nombre",
-                    nonNumeric: "Por favor ingrese un nombre valido",
-                },
-                lastName: {
-                    required: "Por favor ingrese su apellido",
-                    nonNumeric: "Por favor ingrese un apellido valido",
-                },
-                email: {
-                    email: "Por favor ingrese un email valido",
-                    required: "Por favor ingrese su email"
-                },
-                phone: {
-                    number: "Por favor ingrese un teléfono valido",
-                    required: "Por favor ingrese su teléfono"
-                },
-            },
+    rules: {
+        name: {
+            required: true,
+            nonNumeric: true,
+        },
+        lastName: {
+            required: true,
+            nonNumeric: true,
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        phone: {
+            required: true,
+            number: true,
+        },
+    },
+    messages: {
+        name: {
+            required: "Por favor ingrese su nombre",
+            nonNumeric: "Por favor ingrese un nombre valido",
+        },
+        lastName: {
+            required: "Por favor ingrese su apellido",
+            nonNumeric: "Por favor ingrese un apellido valido",
+        },
+        email: {
+            email: "Por favor ingrese un email valido",
+            required: "Por favor ingrese su email"
+        },
+        phone: {
+            number: "Por favor ingrese un teléfono valido",
+            required: "Por favor ingrese su teléfono"
+        },
+    },
+    errorPlacement: function (err, element) {
+        element.attr('data-content', err.text());
+        $(element).popover('show');
+    },
 
-            highlight: function (element) {
-                $(element).closest('.form-group').addClass('has-warning');
-                $(element).addClass('form-control-warning');
-            },
-            unhighlight: function (element) {
-                $(element).closest('.form-group').removeClass('has-warning');
-                $(element).removeClass('form-control-warning');
-            },
-            submitHandler: function (form) {
-                form.submit();
-            },
+    highlight: function (element) {
+        $(element).closest('.form-group').addClass('has-warning');
+        $(element).addClass('form-control-warning');
+    },
+    unhighlight: function (element) {
+        $(element).popover('hide');
+        $(element).closest('.form-group').removeClass('has-warning');
+        $(element).removeClass('form-control-warning');
+    },
+    submitHandler: function (form) {
+        form.submit();
+    },
 
-        });
+});
