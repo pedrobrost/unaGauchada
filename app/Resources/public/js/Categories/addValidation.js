3ec00aa953740bd65de77201c680a9e1d1399465
francoborrelli <francoborrelli96@outlook.com>
2017-07-24 13:45:02 -0300

    funca

--- app/Resources/public/js/Categories/addValidation.js
+++ app/Resources/public/js/Categories/addValidation.js
@@ -1,59 +1,82 @@
-$('.editCategoryForm').each(function() {  // attach to all form elements on page
-        $(this).validate({
-            rules: {
-                message: {
-                    required:true
-            },
-            },
-            messages: {
-                message: {
-                    required: "",
-                },
+var error = false; 
 
+$(document).ready(function () {
+    $('#newCategory').popover({
+        placement: 'left',
+        trigger: 'manual'
+    });
+    $('.category').popover({
+        placement: 'left',
+        trigger: 'manual'
+    });
+});
+
+$('.editCategoryForm').each(function () { // attach to all form elements on page
+    $(this).validate({
+        rules: {
+            message: {
+                required: true
             },
-            errorPlacement: function(error, element) {   },
-            highlight: function (element) {
-                $(element).closest('.form-group').addClass('has-warning');
-                $(element).addClass('form-control-warning');
+        },
+        messages: {
+            message: {
+                required: "Debe ingresar el nombre de la categoria",
             },
-            unhighlight: function (element) {
-                $(element).closest('.form-group').removeClass('has-warning');
-                $(element).removeClass('form-control-warning');
-                $(element).closest('.form-group').addClass('has-success');
-            },
-            submitHandler: function (form) {
-                form.submit();
-            },
-        });
-              });
+
+        },
+        errorPlacement: function (err, element) {
+            element.attr('data-content', err.text());
+            $(element).popover('show');
+        },
+        highlight: function (element) {
+            $(element).closest('.form-group').addClass('has-warning');
+            $(element).addClass('form-control-warning');
+            error = true;
+        },
+        unhighlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-warning');
+            $(element).removeClass('form-control-warning');
+            $(element).closest('.form-group').addClass('has-success');
+            $(element).popover('hide');
+            error = false;
+        },
+        submitHandler: function (form) {
+            form.submit();
+        },
+    });
+});
 
 
 
-$('.addCategoryForm').each(function() {  // attach to all form elements on page
-        $(this).validate({
-            rules: {
-                nombre: {
-                    required:true
+$('.addCategoryForm').each(function () { // attach to all form elements on page
+    $(this).validate({
+        rules: {
+           newCategory: {
+                required: true
             },
+        },
+        messages: {
+            newCategory: {
+                required: "Debe ingresar el nombre de la nueva categoria",
             },
-            messages: {
-                nombre: {
-                    required: "Debe completar este campo",
-                },
 
-            },
-            highlight: function (element) {
-                $(element).closest('.form-group').addClass('has-warning');
-                $(element).addClass('form-control-warning');
-            },
-            unhighlight: function (element) {
-                $(element).closest('.form-group').removeClass('has-warning');
-                $(element).removeClass('form-control-warning');
-                $(element).closest('.form-group').addClass('has-success');
-            },
-            submitHandler: function (form) {
-                form.submit();
-            },
-        });
-              });
-
+        },
+        errorPlacement: function (err, element) {
+            element.attr('data-content', err.text());
+            $(element).popover('show');
+        },
+        highlight: function (element) {
+            $(element).closest('.form-group').addClass('has-warning');
+            $(element).addClass('form-control-warning');
+        },
+        unhighlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-warning');
+            $(element).removeClass('form-control-warning');
+            $(element).closest('.form-group').addClass('has-success');
+            $(element).popover('hide');
+        },
+        submitHandler: function (form) {
+            form.submit();
+        },
+    });
+});
