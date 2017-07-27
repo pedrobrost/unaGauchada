aa4aa587aa71d3f7cb38bdaa087206007b382906
Pedro Brost <pedro.brost97@gmail.com>
2017-07-27 14:36:17 -0300

    Typo

--- src/UnaGauchada/AdminBundle/Controller/AdminController.php
+++ src/UnaGauchada/AdminBundle/Controller/AdminController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
-    public function raitingAction()
+    public function ratingAction()
     {
         return $this->render('AdminBundle:RatingReport:userReport.html.twig');
     }
