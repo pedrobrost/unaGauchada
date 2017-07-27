196ffe54492279647f42dde3fdf8f346dd2fde1a
Pedro Brost <pedro.brost97@gmail.com>
2017-07-27 15:19:38 -0300

    Se implemento el controlador que pasa todos los usuarios

--- src/UnaGauchada/AdminBundle/Controller/AdminController.php
+++ src/UnaGauchada/AdminBundle/Controller/AdminController.php
@@ -3,11 +3,18 @@
 namespace UnaGauchada\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use UnaGauchada\UserBundle\Entity\User;
 
 class AdminController extends Controller
 {
     public function ratingAction()
     {
-        return $this->render('AdminBundle:RatingReport:userReport.html.twig');
+        $repository = $this->getDoctrine()
+            ->getRepository(User::class);
+        $query = $repository->createQueryBuilder('p')
+            ->where('p.isAdmin = 0')
+            ->getQuery();
+        $users = $query->getResult();
+        return $this->render('AdminBundle:RatingReport:userReport.html.twig', array('users' => $users));
     }
 }
