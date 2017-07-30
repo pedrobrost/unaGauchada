fbf5918e837d80478d1a499603583831881be51c
Pedro Brost <pedro.brost97@gmail.com>
2017-07-30 20:45:02 -0300

    category icon

--- src/UnaGauchada/AdminBundle/Controller/AdminController.php
+++ src/UnaGauchada/AdminBundle/Controller/AdminController.php
@@ -39,15 +39,17 @@ class AdminController extends Controller
         $em->flush();
 
         $achievements = $request->request->all();
-        $last = array_pop($achievements);
+        $lastIcon = array_pop($achievements);
+        $lastName = array_pop($achievements);
         $min = PHP_INT_MIN;
         for($i = 1; $i <= (count($achievements) / 3); $i++){
-            $em->persist(new Achievement($achievements['campoNombre'.$i], $min, (int)$achievements['campoRango'.$i]));
+            $em->persist(new Achievement($achievements['campoNombre'.$i], $min, (int)$achievements['campoRango'.$i], $achievements['campoIcono'.$i]));
             $min = (int)$achievements['campoRango'.$i] + 1;
         }
-        $em->persist(new Achievement($last, $min, PHP_INT_MAX));
+        $em->persist(new Achievement($lastName, $min, PHP_INT_MAX, $lastIcon));
 
         $em->flush();
+
         $this->addFlash('notice', 'Los cambios se han guardado correctamente.');
         return $this->redirectToRoute('achievements_management');
     }
