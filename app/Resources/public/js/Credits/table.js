b76910871f069430a219999c5ed63edef6d79ff0
francoborrelli <francoborrelli96@outlook.com>
2017-08-01 11:09:31 -0300

    se arreglo la tabla de ganancias, ordena bien las fechas

--- app/Resources/public/js/Credits/table.js
+++ app/Resources/public/js/Credits/table.js
@@ -1,7 +1,52 @@
+function trim(str) {
+str = str.replace(/^\s+/, '');
+for (var i = str.length - 1; i >= 0; i--) {
+if (/\S/.test(str.charAt(i))) {
+str = str.substring(0, i + 1);
+break;
+}
+}
+return str;
+}
+
+function dateHeight(dateStr){
+if (trim(dateStr) != '') {
+var frDate = trim(dateStr).split(' ');
+var frDateParts = frDate[0].split('/');
+var day = frDateParts[0] * 60 * 24;
+var month = frDateParts[1] * 60 * 24 * 31;
+var year = frDateParts[2] * 60 * 24 * 366;
+var x = day+month+year;
+} else {
+var x = 99999999999999999; //GoHorse!
+}
+return x;
+}
+
+jQuery.fn.dataTableExt.oSort['date-euro-asc'] = function(a, b) {
+var x = dateHeight(a);
+var y = dateHeight(b);
+var z = ((x < y) ? -1 : ((x > y) ? 1 : 0));
+return z;
+};
+
+jQuery.fn.dataTableExt.oSort['date-euro-desc'] = function(a, b) {
+var x = dateHeight(a);
+var y = dateHeight(b);
+var z = ((x < y) ? 1 : ((x > y) ? -1 : 0));
+return z;
+};
+
 $(document).ready(function () {
     $('#ganancias').DataTable({
+"aoColumns": [
+null,
+{ "sType": "date-euro"},
+null,
+null
+],
         "order": [
-            [1, "asc"]
+            [1, "desc"]
         ],
         "bLengthChange": false,
         "info": false,
